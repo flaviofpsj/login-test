9db9900cd0101ab3095be60857fce47eb8f81317
Flavio Farias <flavio@engapp.com.br>
2018-11-21 16:44:58 -0300

    update validation

--- lib/src/mixins/validation.dart
+++ lib/src/mixins/validation.dart
@@ -1,5 +1,18 @@
 class Validation {
 
+  //username validator
+  String validateUsername (String value) {
+    if(value.length == 0) {
+      return 'Fill in this field';
+    }
+    else if(value.length <= 4) {
+      return 'Incorrect username';
+    }
+    else {
+      return null;
+    }
+  }
+
   //email validator
   String validateEmail (String value) {
     if(value.length == 0) {
