5c7d91132d466a3b4a953d0b4e888faf7820e0ce
Flavio Farias <flavio@engapp.com.br>
2018-11-21 16:46:32 -0300

    update theme application

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,9 +7,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context){
     return new MaterialApp(
       title: 'Login Test',
+      //hide debug banner
       debugShowCheckedModeBanner: false,
+      //theme of application
       theme: new ThemeData(
         primaryColor: Colors.pinkAccent,
+        primarySwatch: pinkAccent,
         cursorColor: Colors.pinkAccent,
         buttonColor: Colors.pinkAccent,
         buttonTheme: new ButtonThemeData(
@@ -17,11 +20,32 @@ class App extends StatelessWidget {
         ),
         errorColor: Colors.cyan,
       ),
-      routes: <String, WidgetBuilder>{
-        '/loginScreen': (BuildContext context) => new LoginScreen(),
-        '/secondScreen': (BuildContext context) => new SecondScreen(),
-      },
+      routes: screenRoutes,
       home: new LoginScreen(),
     );
   }
 }
+
+//create routes
+final screenRoutes = <String, WidgetBuilder>{
+  //route names coming from their respective classes
+  LoginScreen.route: (BuildContext context) => new LoginScreen(),
+  SecondScreen.route: (BuildContext context) => new SecondScreen(response: null,),
+};
+
+//create material color to assign to primarySwatch
+const MaterialColor pinkAccent = const MaterialColor(
+  0xFFFF4081,
+  const <int, Color>{
+    50: const Color(0xFFFF4081),
+    100: const Color(0xFFFF4081),
+    200: const Color(0xFFFF4081),
+    300: const Color(0xFFFF4081),
+    400: const Color(0xFFFF4081),
+    500: const Color(0xFFFF4081),
+    600: const Color(0xFFFF4081),
+    700: const Color(0xFFFF4081),
+    800: const Color(0xFFFF4081),
+    900: const Color(0xFFFF4081),
+  }
+);
