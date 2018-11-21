cfc5e1390e4d0420865028dd07ee8c54c44abd92
Flavio Farias <flavio@engapp.com.br>
2018-11-21 16:44:09 -0300

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,6 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Tests here
+    //test here
   });
 }
