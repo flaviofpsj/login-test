059b00fc0838292407aa14ba98f5d79238624360
Flavio Farias <flavio@engapp.com.br>
2018-11-21 16:48:03 -0300

    update second screen layout

--- lib/src/components/login/second_screen.dart
+++ lib/src/components/login/second_screen.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
+import './../models/login_model.dart';
 
 class SecondScreen extends StatefulWidget {
-  @override
+  //declare a field that holds the Response
+  final ResponseBody response;
+
+  //in the constructor, require a Response
+  SecondScreen({Key key, @required this.response}) : super(key: key);
+
+  //route to second screen
+  static String route = '/secondScreen';
+
+  //@override
   _SecondScreenState createState() => _SecondScreenState();
 }
 
 class _SecondScreenState extends State<SecondScreen> {
+
+  //style local variables declarated
+  var usernameStyle = new TextStyle(color: Colors.black, fontSize: 40.0, fontWeight: FontWeight.w100,);
+  var textStyle = new TextStyle(color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.w100,);
+  var boldTextStyle = new TextStyle(fontWeight: FontWeight.w300,);
+
+  //icon builder
+  Widget iconBuilder() {
+    return new Icon(
+      Icons.sentiment_very_satisfied,
+      color: Colors.pinkAccent,
+      size: 200.0,
+    );
+  }
+
+  //username builder
+  Widget usernameTextBuilder() {
+    return new Container(
+      padding: new EdgeInsets.only(bottom: 12.0,),
+      child: new RichText(
+        text: new TextSpan(
+          text: 'Hello, ',
+          style: usernameStyle,
+          children: <TextSpan>[
+            new TextSpan(
+              text: '${widget.response.username}',
+              style: boldTextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //text builder
+  Widget textBuilder(IconData icon, String firstText, String secondText) {
+    return Container(
+      padding: new EdgeInsets.only(bottom: 8.0,),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.only(right: 5.0),
+            child: new Icon(
+              icon,
+              color: Colors.pinkAccent,
+              size: 20.0,
+            ),
+          ),
+          new RichText(
+            text: new TextSpan(
+              text: firstText,
+              style: textStyle,
+              children: <TextSpan>[
+                new TextSpan(
+                  text: secondText,
+                  style: boldTextStyle,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -17,12 +93,13 @@ class _SecondScreenState extends State<SecondScreen> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Icon(
-                Icons.sentiment_very_satisfied,
-                color: Colors.pinkAccent,
-                size: 200.0,
-              ),
-              new Text('Hello', style: new TextStyle(fontSize: 45.0, fontWeight: FontWeight.w100),),
+              iconBuilder(),
+              usernameTextBuilder(),
+              //widget is the current configuration. A State object's configuration
+              //is the corresponding StatefulWidget instance.
+              textBuilder(Icons.email, 'Your Email is: ', '${widget.response.email}'),
+              textBuilder(Icons.lock, 'Your Password is: ', '${widget.response.password}'),
+              textBuilder(Icons.vpn_key, 'Your Access Token is: ', '${widget.response.accessToken}'),
             ],
           ),
         ),
